f761986191a1a780c063b7de6191c1d831cb687d
ytallo <nexusstealth312@gmail.com>
2025-06-13 16:36:11 -0300

    Update script.js

---
+++ public/script.js
@@ -0,0 +1,7 @@
+// Modo escuro/claro
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark');
+  }
+});
