71a9d1325fdde1a9dc4b2769fee9dedd797a5ab6
ytallo <nexusstealth312@gmail.com>
2025-06-13 17:46:16 -0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const firebase = require('./config/firebase');
+const iaRoute = require('./routes/ia');
+const uploadRoute = require('./routes/upload');
+const deployRoute = require('./routes/deploy');
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/', require('./routes/pages'));
+app.use('/upload', uploadRoute);
+app.use('/deploy', deployRoute);
+app.use('/ia', iaRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
